a34745284ad78442563db9cc759e5a813705b072
Amrit Rau <amritrau@users.noreply.github.com>
2015-11-16 11:04:52 -0800

    Wrote the takeTurn() helper method

--- Crunch.java
+++ Crunch.java
@@ -1,1 +1,19 @@
-
+public int takeTurn(int player, int sticks)
+{
+	if (player == 0)
+	{
+	  int s = 99;
+	  while (s > sticks)
+	  {
+  	  System.out.println("How many sticks do you want to take?");
+  	  Scanner scan = new Scanner(System.in);
+  	  s = scan.nextInt();
+	  }
+	  return s;
+	}
+	else
+	{
+    if (sticks < 2) return 1;
+    else  return 1 + (int)(Math.random() + 0.5);
+	}
+}
